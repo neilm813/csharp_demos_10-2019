3e6efb60c1b97a894477cb6eb5a9224ffc57c8b6
Neil Mosunic <neilm813@gmail.com>
2019-10-10 16:42:43 -0700

    part 2 of 1 to many

---
+++ w2/d4/ForumDemo/Models/Post.cs
@@ -0,0 +1,29 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ForumDemo.Models
+{
+    public class Post
+    {
+        [Key] // Primary Key
+        public int PostId { get; set; }
+
+        // Foreign Key 
+        public int UserId { get; set; }
+        
+        // Navigation property for the relationship
+        public User Author {get; set; } // 1 user related to each Post
+
+        [Required]
+        [MinLength(2, ErrorMessage = "Must be more than 2 characters.")]
+        public string Topic { get; set; }
+
+        [Required]
+        [MinLength(3, ErrorMessage = "Must be more than 2 characters.")]
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+    }
+}
